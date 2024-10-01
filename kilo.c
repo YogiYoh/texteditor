8c3a0f729666a9deff233e177ebafe41567f8e57
YogiYoh <chenalvin2022@gmail.com>
2024-10-01 12:58:00 -0700

    Turning off/on more attributes (input, control, output) Turned Off:     OPOST - Output Processing (Carriage Return/Newline)     Miscellaneous Flag - terminal control that may or may not be significant.         BKRNT         INCPK         ISTRIP         CS8 Added more comments to add more clarity. Makes it easier for me and others to understand the logic/fuctionality of code.

--- kilo.c
+++ kilo.c
@@ -16,10 +16,10 @@ void enableRaw(){
   atexit(disableRaw); // registers the function disableRaw to be later use when program exits
   
   struct termios raw = default_att; // Creates a COPY of default_att 
-
-  raw.c_iflag &= ~(IXON | ICRNL); // Edits INPUT attributes (determines how data coming from the keyboard is handled) by flipping bits (~) and adding them to raw struct (&=), to turn off certain signals  
+  raw.c_oflag &= ~(OPOST); // Edits OUTPUT attributes (determines how data going to the screen is processed before being displayed) by flipping bits (~) and adding them to raw struct (&=), to turn off certain signals 
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP); // Edits INPUT attributes (determines how data coming from the keyboard is handled) by flipping bits (~) and adding them to raw struct (&=), to turn off certain signals  
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // Edits local attributes (how should the terminal behave while you're typing) by flipping the bits (~) and adding them to raw struct (&=), to turn off certain signals  
-
+  raw.c_cflag |= (CS8); // Edits CONTROL attributes (dictate how data travels (format, error checks, flow)). The following ENABLES CS8 which make terminal specify each character received/outgoing data as 8 bits
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // Sets custom attributes in raw into the standard input 
 }
 
@@ -30,9 +30,9 @@ int main() {
   char c;
   while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
       if(iscntrl(c)){
-        printf("%d\n", c);  // Prints control character (backspace, spacebar, etc)
+        printf("%d\r\n", c);  // Prints control character (backspace, spacebar, etc)
       }else{
-        printf("%d ('%c')\n", c, c); // Print non control characters with its associated ASCII value
+        printf("%d ('%c')\r\n", c, c); // Print non control characters with its associated ASCII value
       }
   }
   return 0;
